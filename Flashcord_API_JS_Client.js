9373cd9524be2529d34fb01e4eff472aa4b602b8
Sirius B <root.siriusb@gmail.com>
2024-02-03 15:52:29 +0100

    Final API V3 Client

--- Flashcord_API_JS_Client.js
+++ Flashcord_API_JS_Client.js
@@ -2,37 +2,31 @@
  and connects to a WebSocket to raw TCP translation server */
 
 function Flashcord_API_Client(API_Request) {
-    //const ServerAddress = new WebSocket("ws://aura-two.sirio-network.com:1407");
-    const ServerAddress = new WebSocket("ws://localhost:1407");
-    ServerAddress.onopen = function(e) {
-        console.log("Sending API Version Number")
-        ServerAddress.send("3.0");
-    };
+    return new Promise(function(Success) {
+        //const ServerAddress = new WebSocket("ws://aura-two.sirio-network.com:1407");
+        const ServerAddress = new WebSocket("ws://localhost:1407");
+        ServerAddress.onopen = function(e) {
+            console.log("Sending API Version Number")
+            ServerAddress.send("3.0");
+        };
 
-    ServerAddress.onmessage = function(event) {
-        if (event.data == "OK" || event.data == "OUTDATED_SERVER") {
-            console.log(event.data);
-            ServerAddress.send(API_Request);
-        } else {
-            console.log(event.data);
-            ServerAddress.close();
-            return event;
+        ServerAddress.onmessage = function(event) {
+            if (event.data == "OK" || event.data == "OUTDATED_SERVER") {
+                ServerAddress.send(API_Request);
+            } else {
+                ServerAddress.close();
+                Success(event.data);
+            }
         }
-        
-    }
-
-    ServerAddress.onclose = function(event) {
-        console.log("Connection closed");
-    }
+        ServerAddress.onclose = function(event) { console.log("Connection closed"); }
+    });
 };
 
 async function FlashstoreAPI_Requester() {
     RequestBox = document.getElementById("API_Input");
     ResultBox = document.getElementById("API_Result");
     API_Request = RequestBox.value;
-    Result = Flashcord_API_Client(API_Request);
-    console.log(Result)
-    Result = "Result: " + Result;
+    Result = "Result: " + await Flashcord_API_Client(API_Request);
     ResultBox.innerText = Result;
     console.log(Result);
 };
