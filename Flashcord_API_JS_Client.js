1dc370f15b17c2e48b14e8049b30961d3ad702ca
Sirius B <root.siriusb@gmail.com>
2024-02-05 13:42:06 +0100

    h

--- Flashcord_API_JS_Client.js
+++ Flashcord_API_JS_Client.js
@@ -3,8 +3,8 @@
 
 function Flashcord_API_Client(API_Request) {
     return new Promise(function(Success) {
-        //const ServerAddress = new WebSocket("ws://aura-two.sirio-network.com:1407");
-        const ServerAddress = new WebSocket("ws://localhost:1407");
+        let ServerAddress = new WebSocket("wss://aura-two.sirio-network.com:1408");
+        // let ServerAddress = new WebSocket("ws://localhost:1407");
         ServerAddress.onopen = function(e) {
             console.log("Sending API Version Number")
             ServerAddress.send("3.0");
@@ -18,7 +18,13 @@ function Flashcord_API_Client(API_Request) {
                 Success(event.data);
             }
         }
-        ServerAddress.onclose = function(event) { console.log("Connection closed"); }
+        ServerAddress.onclose = function(event) {
+            if (event.wasClean) {
+                console.log("Connection closed. Code=${event.code} Reason=${event.reason}");
+            } else {
+                console.log("Connection closed forcefully.");
+            }
+        }
     });
 };
 
